aae5270ef769a9b4fbc4521771750231c36a2cab
Man4ester <byrkovskiy@gmail.com>
2016-01-08 17:35:35 +0200

    configuration and test point

--- MongoDBUtils/src/it/bismark/central/Main.java
+++ MongoDBUtils/src/it/bismark/central/Main.java
@@ -1,9 +1,34 @@
 package it.bismark.central;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
 public class Main {
-	
+
+	private static final Logger LOGGER = Logger.getLogger(Main.class);
+
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		BasicConfigurator.configure();
+		LOGGER.debug("Start point");
+		MongoClient client = new MongoClient();
+		try {
+			MongoDatabase db = client.getDatabase("test");
+			MongoCollection<Document> person = db.getCollection("person");
+			Document p = new Document();
+			p.append("_id", 12);
+			p.append("name", "Petya");
+			person.insertOne(p);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			client.close();
+		}
+
 	}
 
 }
